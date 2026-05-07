a045e65e3524a90adaeaea5f8b49cf2fb2086be2
ousama lasri <oussama.lasri.2k@gmail.com>
2026-05-07 13:17:23 +0100

    feat: implement authentication and authorization routes and middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 import connectDB from "./config/db.js";
 
@@ -22,3 +23,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.use("/api/auth", authRoutes);
